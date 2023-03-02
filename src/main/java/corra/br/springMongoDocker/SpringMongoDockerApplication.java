b33115df35a8bf36d4ee1c7d399302beb37cd7b2
PHCorra <ph.corra@gmail.com>
2023-03-01 23:41:16 -0300

    feat: Creating the mongodb Docker instance and testing the connection with the java application

---
+++ src/main/java/corra/br/springMongoDocker/SpringMongoDockerApplication.java
@@ -0,0 +1,13 @@
+package corra.br.springMongoDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoDockerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/corra/br/springMongoDocker/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package corra.br.springMongoDocker.repositories;
+
+import corra.br.springMongoDocker.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
